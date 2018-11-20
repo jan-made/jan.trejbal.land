5035f35caee0d8fc74a76cef9bd3f93529c65f0d
Jan Trejbal <jan.trejbal@gmail.com>
2018-11-21 00:04:14 +0100

    Prepare CV project

---
+++ generatepdf.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var pdf = require('html-pdf');
+var wkhtmltopdf = require('wkhtmltopdf');
+var html = fs.readFileSync('./public/index.html', 'utf8');
+var options = {
+    "format": 'A4',
+    "border": {
+        "top": "1cm",            // default is 0, units: mm, cm, in, px
+        "right": "3cm",
+        "bottom": "2cm",
+        "left": "3cm"
+    }
+};
+
+wkhtmltopdf(html, {output: './static/cv.pdf'});
+/* pdf.create(html, options).toFile('./static/cv.pdf', function (err, res) {
+    if (err) return console.log(err);
+    console.log(res);
+});*/
